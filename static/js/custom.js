3a0f6e35684bb844a9ca553d2474ba660351ed8f
meysam-Github <rmeysam891@gmail.com>
2024-10-22 16:26:29 +0330

    ajax update

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,13 +1,22 @@
-function sendArticleComment(){
+function sendArticleComment(articleId){
     // console.log('submit article comment');
     var comment = $('#commentText').val();
+    var parentId = $('#parent_id').val();
     
+
     $.get('/articles/add-article-comment', {
-        articleComment : comment,
-        articleId : 32,
-        parentId : null,
+        article_comment: comment,
+        article_id: articleId,
+        parent_id: parentId,
 
     }).then(res => {
-        console.log(res)
-    });
+        console.log(res);
+        location.reload();
+        
+    })
+}
+
+function fillParnetId(parentId){
+    $('#parent_id').val(parentId);
+    document.getElementById('comment_form').scrollIntoView( {behavior: "smooth"} );
 }
